fe40fb842bc4b39426b06ca45ce72a140809d297
Mahamud Aden <mahamudadn@gmail.com>
2023-06-02 15:54:52 -0500

    added a header jsx & css component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
+import axios from 'axios'
+import Header from '../Header/Header'
 import './App.css';
 
 function App() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Gallery of My Life</h1>
-        </header>
-        <p>Gallery goes here</p>
-        <img src="images/goat_small.jpg"/>
-      </div>
+     <Header/>
     );
 }
 
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,20 @@
+import react from 'react'
+import './Header.css';
+
+function Header() {
+    return (
+        <>
+        <div className="App">
+    <header className="App-header">
+      <h1 className="App-title">Gallery of My Life</h1>
+    </header>
+    <p>Gallery goes here</p>
+    <img src="images/goat_small.jpg"/>
+  </div>
+  </>
+    )
+    
+
+}
+
+export default Header;
